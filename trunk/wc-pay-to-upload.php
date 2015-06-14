c793c576ffaf50cfb9cb43cfbd6a17587ea1dd94
wpashokg <wpashokg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-14 17:31:11 +0000

    test info updatde for latest version
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pay-to-upload-modification/tags/2.0.2@1180679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/wc-pay-to-upload.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: WooCommerce Pay to Upload
+Plugin URI:  http://ashokg.in/woocommerce-pay-to-upload 
+Description: Allow customers to upload a file after the payment made. Developed on the Airy plugin framework
+Version: 2.0.2
+Author: Ashok G
+Author URI: http://ashokg.in
+
+Copyright: © 2014 Ashok G.
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Special thanks to Michael McCarthy PhD ( @WPC4U ) for having this plugin developed and allowing it to be shared.
+*/
+
+/**
+ * Required files
+ **/
+require_once('classes/class-airy-framework.php' );
+require_once('classes/class-wc-pay-to-upload.php' );
+
+/**
+ * Start the engines!
+ **/
+$WC_Pay_To_Upload = new WC_Pay_To_Upload;
